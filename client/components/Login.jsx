984dd8db2a6c8b6c72462b65e5adf860c25ada88
Yuzuki-S <yuzuki.sadoh@gmail.com>
2018-09-09 16:02:22 +1200

    Login page

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,12 +1,58 @@
 import React from 'react'
+import { Button, FormGroup, FormControl, ControlLabel, Form, Col, Checkbox } from "react-bootstrap";
 
-function Login () {
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  render() {
+
+ 
   return (
     <div className='box'>
-      <h2>LOGIN!!!!!!!!!!!!!!!!!!!!!!!</h2>
+            <div className="Login">
+            <h2>Log in </h2>
+            <Form horizontal>
+  <FormGroup controlId="formHorizontalEmail">
+    <Col componentClass={ControlLabel} sm={2}>
+      Email
+    </Col>
+    <Col sm={10}>
+      <FormControl type="email" placeholder="Email" />
+    </Col>
+  </FormGroup>
+
+  <FormGroup controlId="formHorizontalPassword">
+    <Col componentClass={ControlLabel} sm={2}>
+      Password
+    </Col>
+    <Col sm={10}>
+      <FormControl type="password" placeholder="Password" />
+    </Col>
+  </FormGroup>
+
+  <FormGroup>
+    <Col smOffset={2} sm={10}>
+      <Checkbox>Remember me</Checkbox>
+    </Col>
+  </FormGroup>
+
+  <FormGroup>
+    <Col smOffset={2} sm={10}>
+      <Button type="submit">Sign in</Button>
+    </Col>
+  </FormGroup>
+</Form>;
+      </div>
      
     </div>
   )
 }
+ }
 
 export default Login
--- public/bundle.js
+++ public/bundle.js
@@ -44947,23 +44947,118 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _reactBootstrap = __webpack_require__(19);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function Login() {
-  return _react2.default.createElement(
-    'div',
-    { className: 'box' },
-    _react2.default.createElement(
-      'h2',
-      null,
-      'LOGIN!!!!!!!!!!!!!!!!!!!!!!!'
-    )
-  );
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Login = function (_React$Component) {
+  _inherits(Login, _React$Component);
+
+  function Login(props) {
+    _classCallCheck(this, Login);
+
+    var _this = _possibleConstructorReturn(this, (Login.__proto__ || Object.getPrototypeOf(Login)).call(this, props));
+
+    _this.state = {
+      email: "",
+      password: ""
+    };
+    return _this;
+  }
+
+  _createClass(Login, [{
+    key: "render",
+    value: function render() {
+
+      return _react2.default.createElement(
+        "div",
+        { className: "box" },
+        _react2.default.createElement(
+          "div",
+          { className: "Login" },
+          _react2.default.createElement(
+            "h2",
+            null,
+            "Log in "
+          ),
+          _react2.default.createElement(
+            _reactBootstrap.Form,
+            { horizontal: true },
+            _react2.default.createElement(
+              _reactBootstrap.FormGroup,
+              { controlId: "formHorizontalEmail" },
+              _react2.default.createElement(
+                _reactBootstrap.Col,
+                { componentClass: _reactBootstrap.ControlLabel, sm: 2 },
+                "Email"
+              ),
+              _react2.default.createElement(
+                _reactBootstrap.Col,
+                { sm: 10 },
+                _react2.default.createElement(_reactBootstrap.FormControl, { type: "email", placeholder: "Email" })
+              )
+            ),
+            _react2.default.createElement(
+              _reactBootstrap.FormGroup,
+              { controlId: "formHorizontalPassword" },
+              _react2.default.createElement(
+                _reactBootstrap.Col,
+                { componentClass: _reactBootstrap.ControlLabel, sm: 2 },
+                "Password"
+              ),
+              _react2.default.createElement(
+                _reactBootstrap.Col,
+                { sm: 10 },
+                _react2.default.createElement(_reactBootstrap.FormControl, { type: "password", placeholder: "Password" })
+              )
+            ),
+            _react2.default.createElement(
+              _reactBootstrap.FormGroup,
+              null,
+              _react2.default.createElement(
+                _reactBootstrap.Col,
+                { smOffset: 2, sm: 10 },
+                _react2.default.createElement(
+                  _reactBootstrap.Checkbox,
+                  null,
+                  "Remember me"
+                )
+              )
+            ),
+            _react2.default.createElement(
+              _reactBootstrap.FormGroup,
+              null,
+              _react2.default.createElement(
+                _reactBootstrap.Col,
+                { smOffset: 2, sm: 10 },
+                _react2.default.createElement(
+                  _reactBootstrap.Button,
+                  { type: "submit" },
+                  "Sign in"
+                )
+              )
+            )
+          ),
+          ";"
+        )
+      );
+    }
+  }]);
+
+  return Login;
+}(_react2.default.Component);
 
 exports.default = Login;
 
